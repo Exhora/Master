22ead952b0fc0bf0cbf2e91a9e792e24416b2383
Taiane Coelho Ramos <exhora.tat@gmail.com>
2015-10-04 21:57:26 -0300

    calcula os clusters

--- init.R
+++ init.R
@@ -1,6 +1,7 @@
 load('zv_sfnwmrda_cc400_tc100_SITEdxGroup_spCorr.RData')
 
 require(igraph)
+require(kernlab)
 
 pos = which(funcIdade < 30 & dxGroup == 2)
 
@@ -8,3 +9,4 @@ dados = dataset[pos]
 
 media_dados = colMeans(dados)
 
+value = specc(media_dados, 5) #usando 5 clusters
