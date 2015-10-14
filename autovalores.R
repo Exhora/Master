010c05654f60811a4c53051312b98373ea831bc9
Taiane Coelho Ramos <exhora.tat@gmail.com>
2015-10-14 15:19:44 -0300

    calculo dos autovalores e regressao linear

--- autovalores.R
+++ autovalores.R
@@ -1,6 +1,54 @@
-eigenValues = array(dim=c(dim(dados)[1], 316))
 
-for(i in 1:dim(dados)[1]){
-	eigenValues[i,] = eigen(dados[i,,], TRUE, only.values=TRUE)$values
+numPacientes = dim(cluster1)[1]
+
+eigenValues1 = array(dim=c(numPacientes, dim(cluster1)[2]))
+eigenValues2 = array(dim=c(numPacientes, dim(cluster2)[2]))
+eigenValues3 = array(dim=c(numPacientes, dim(cluster3)[2]))
+eigenValues4 = array(dim=c(numPacientes, dim(cluster4)[2]))
+eigenValues5 = array(dim=c(numPacientes, dim(cluster5)[2]))
+
+for(i in 1:numPacientes){
+	eigenValues1[i,] = eigen(cluster1[i,,], only.values=TRUE)$values
 }
+for(i in 1:numPacientes){
+	eigenValues2[i,] = eigen(cluster2[i,,], only.values=TRUE)$values
+}
+for(i in 1:numPacientes){
+	eigenValues3[i,] = eigen(cluster3[i,,], only.values=TRUE)$values
+}
+
+for(i in 1:numPacientes){
+	eigenValues4[i,] = eigen(cluster4[i,,], only.values=TRUE)$values
+}
+
+for(i in 1:numPacientes){
+	eigenValues5[i,] = eigen(cluster5[i,,], only.values=TRUE)$values
+}
+
+MaiorAutovalor1 =  NULL
+MaiorAutovalor2 =  NULL
+MaiorAutovalor3 =  NULL
+MaiorAutovalor4 =  NULL
+MaiorAutovalor5 =  NULL
+
+for(i in 1:numPacientes){
+	MaiorAutovalor1[i] = max(eigenValues1[i,])
+}
+for(i in 1:numPacientes){
+	MaiorAutovalor2[i] = max(eigenValues2[i,])
+}
+for(i in 1:numPacientes){
+	MaiorAutovalor3[i] = max(eigenValues3[i,])
+}
+for(i in 1:numPacientes){
+	MaiorAutovalor4[i] = max(eigenValues4[i,])
+}
+for(i in 1:numPacientes){
+	MaiorAutovalor5[i] = max(eigenValues5[i,])
+}
+
+
+
+
+
 
---
+++ regressao.R
@@ -0,0 +1,7 @@
+Idade = funcIdade[pos]
+Sexo = funcSexo[pos]
+summary(lm(MaiorAutovalor1~Idade+Sexo))
+summary(lm(MaiorAutovalor2~Idade+Sexo))
+summary(lm(MaiorAutovalor3~Idade+Sexo))
+summary(lm(MaiorAutovalor4~Idade+Sexo))
+summary(lm(MaiorAutovalor5~Idade+Sexo))
